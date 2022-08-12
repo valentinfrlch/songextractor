ef6e51eb72fabf9451b1e3ef4d3fcc5ea07e38ec
valentinfrlch <valentin.froehlich@gmail.com>
2022-08-12 20:14:15 +0200

    small optimizitations

---
+++ main.py
@@ -0,0 +1,59 @@
+from ShazamAPI import Shazam
+from youtubesearchpython import VideosSearch
+import youtube_dl
+import os
+
+
+def find(query, limit=10):
+    videosSearch = VideosSearch(query, limit=limit)
+    video_ids = []
+    for video in videosSearch.result()["result"]:
+        video_ids.append(video["id"])
+    return video_ids
+
+
+def constructURL(video_ids):
+    url = "https://www.youtube.com/watch?v="
+    return [url + id for id in video_ids]
+
+
+def downloadMP3(url):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    """get mp3 file path in current directory"""
+    mp3_file = [file for file in os.listdir() if file.endswith(".mp3")][0]
+    return mp3_file
+
+
+def recognize(file):
+    bytemp3 = open(file, "rb").read()
+    shazam = Shazam(bytemp3)
+    recognize_generator = shazam.recognizeSong()
+    print("recognizing...")
+    while True:
+        try:
+            return (next(recognize_generator)[1]["track"]["title"])
+            break
+        except Exception as e:
+            continue
+
+
+def main(query, limit=10):
+    print("querying YouTube...")
+    IDs = find(query, limit)
+    URLs = constructURL(IDs)
+    titles = []
+    for url in URLs:
+        path = downloadMP3(url)
+        title = recognize(path)
+        titles.append(title)
+        os.remove(path)
+    return titles
